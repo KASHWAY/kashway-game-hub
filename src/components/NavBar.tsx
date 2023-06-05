5e791ca3b0d2fb3faec6f1adacb3af646c5ed30c
KASHWAY <targosz.hubert@gmail.com>
2023-06-05 14:45:07 +0200

    Creating Colormode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logoGameHub from "../assets/logo-game-hub.jpeg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logoGameHub} boxSize="60px"></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
