a4e1f35b039cbfbc671db6a01fd51a39bf68bc31
KASHWAY <targosz.hubert@gmail.com>
2023-06-06 10:50:48 +0200

    Deleting imports

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,11 +3,9 @@ import {
   HStack,
   Heading,
   Image,
-  Link,
   List,
   ListItem,
   Spinner,
-  Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
