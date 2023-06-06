bdbce504ae9ad3cb7f1a5ebab711fd412c957f47
KASHWAY <targosz.hubert@gmail.com>
2023-06-06 09:30:39 +0200

    REFACTORING: extracting query objects

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -14,11 +15,11 @@ export interface Game {
     metacritic: number;
 }
   
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+const useGames = (gameQuery: GameQuery) => 
   useData<Game>('/games', {
     params: {
-      genres: selectedGenre?.id, platforms: selectedPlatform?.id
+      genres: gameQuery.genre, platforms: gameQuery.platform
     }},
     
-    [selectedGenre?.id, selectedPlatform?.id]);
+    [gameQuery]);
 export default useGames;
